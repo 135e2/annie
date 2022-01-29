iawia002/lux

Add support for ximalaya (#1005)
* download audio from ximalaya

* readme ximalaya

* Update extractors/ximalaya/ximalaya.go

Co-authored-by: Xinzhao Xu <z2d@jifangcheng.com>

* add audio column to README

* update table format

* use ext from url

Co-authored-by: Xinzhao Xu <z2d@jifangcheng.com>

extractors/extractors.go
@@ -33,6 +33,7 @@ import (
 	"github.com/iawia002/lux/extractors/universal"
 	"github.com/iawia002/lux/extractors/vimeo"
 	"github.com/iawia002/lux/extractors/weibo"
+	"github.com/iawia002/lux/extractors/ximalaya"
 	"github.com/iawia002/lux/extractors/xvideos"
 	"github.com/iawia002/lux/extractors/yinyuetai"
 	"github.com/iawia002/lux/extractors/youku"
@@ -69,6 +70,7 @@ func init() {
 		"miaopai":    miaopai.New(),
 		"163":        netease.New(),
 		"weibo":      weibo.New(),
+		"ximalaya":   ximalaya.New(),
 		"instagram":  instagram.New(),
 		"twitter":    twitter.New(),
 		"qq":         qq.New(),

extractors/ximalaya/ximalaya.go
@@ -1,0 +1,83 @@
+package ximalaya
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/iawia002/lux/extractors/types"
+	"github.com/iawia002/lux/parser"
+	"github.com/iawia002/lux/request"
+	"github.com/iawia002/lux/utils"
+)
+
+type extractor struct{}
+
+// New returns a ximalaya extractor.
+func New() types.Extractor {
+	return &extractor{}
+}
+
+// Extract is the main function to extract the data.
+func (e *extractor) Extract(url string, option types.Options) ([]*types.Data, error) {
+	html, err := request.Get(url, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// get the title
+	doc, err := parser.GetDoc(html)
+	if err != nil {
+		return nil, err
+	}
+	title := parser.Title(doc)
+
+	itemIds := utils.MatchOneOf(url, `/sound/(\d+)`)
+	if len(itemIds) == 0 {
+		return nil, errors.New("unable to get audio ID")
+	}
+	if itemIds == nil || len(itemIds) < 2 {
+		return nil, types.ErrURLParseFailed
+	}
+	itemId := itemIds[len(itemIds)-1]
+
+	jsonData, err := request.Get("https://www.ximalaya.com/revision/play/v1/audio?id="+itemId+"&ptype=1", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	var ximalaya ximalayaData
+	if err = json.Unmarshal([]byte(jsonData), &ximalaya); err != nil {
+		return nil, err
+	}
+
+	realURL := ximalaya.Data.Src
+	urlData := make([]*types.Part, 0)
+	totalSize, err := request.Size(realURL, url)
+	if err != nil {
+		return nil, err
+	}
+	_, ext, err := utils.GetNameAndExt(realURL)
+	if err != nil {
+		return nil, err
+	}
+	urlData = append(urlData, &types.Part{
+		URL:  realURL,
+		Size: totalSize,
+		Ext:  ext,
+	})
+	streams := map[string]*types.Stream{
+		"default": {
+			Parts: urlData,
+			Size:  totalSize,
+		},
+	}
+
+	return []*types.Data{
+		{
+			Site:    "喜马拉雅 ximalaya.com",
+			Title:   title,
+			Type:    types.DataTypeAudio,
+			Streams: streams,
+			URL:     url,
+		},
+	}, nil
+}

extractors/ximalaya/ximalaya_test.go
@@ -1,0 +1,30 @@
+package ximalaya
+
+import (
+	"testing"
+
+	"github.com/iawia002/lux/extractors/types"
+	"github.com/iawia002/lux/test"
+)
+
+func TestDownload(t *testing.T) {
+	tests := []struct {
+		name string
+		args test.Args
+	}{
+		{
+			name: "normal test",
+			args: test.Args{
+				URL:   "https://www.ximalaya.com/sound/211583675",
+				Title: "狼的眼睛为什么会发光 - 十万个为什么【宝宝巴士百科故事】",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			data, err := New().Extract(tt.args.URL, types.Options{})
+			test.CheckError(t, err)
+			test.Check(t, tt.args, data[0])
+		})
+	}
+}

135e2/annie

Conflicts:
	README.md
	extractors/extractors.go
	extractors/ximalaya/ximalaya.go
	extractors/ximalaya/ximalaya_test.go

extractors/extractors.go
@@ -33,6 +33,7 @@ import (
 	"github.com/iawia002/annie/extractors/universal"
 	"github.com/iawia002/annie/extractors/vimeo"
 	"github.com/iawia002/annie/extractors/weibo"
+	"github.com/iawia002/annie/extractors/ximalaya"
 	"github.com/iawia002/annie/extractors/xvideos"
 	"github.com/iawia002/annie/extractors/yinyuetai"
 	"github.com/iawia002/annie/extractors/youku"
@@ -69,6 +70,7 @@ func init() {
 		"miaopai":    miaopai.New(),
 		"163":        netease.New(),
 		"weibo":      weibo.New(),
+		"ximalaya":   ximalaya.New(),
 		"instagram":  instagram.New(),
 		"twitter":    twitter.New(),
 		"qq":         qq.New(),

extractors/ximalaya/ximalaya.go
@@ -1,0 +1,83 @@
+package ximalaya
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/iawia002/annie/extractors/types"
+	"github.com/iawia002/annie/parser"
+	"github.com/iawia002/annie/request"
+	"github.com/iawia002/annie/utils"
+)
+
+type extractor struct{}
+
+// New returns a ximalaya extractor.
+func New() types.Extractor {
+	return &extractor{}
+}
+
+// Extract is the main function to extract the data.
+func (e *extractor) Extract(url string, option types.Options) ([]*types.Data, error) {
+	html, err := request.Get(url, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// get the title
+	doc, err := parser.GetDoc(html)
+	if err != nil {
+		return nil, err
+	}
+	title := parser.Title(doc)
+
+	itemIds := utils.MatchOneOf(url, `/sound/(\d+)`)
+	if len(itemIds) == 0 {
+		return nil, errors.New("unable to get audio ID")
+	}
+	if itemIds == nil || len(itemIds) < 2 {
+		return nil, types.ErrURLParseFailed
+	}
+	itemId := itemIds[len(itemIds)-1]
+
+	jsonData, err := request.Get("https://www.ximalaya.com/revision/play/v1/audio?id="+itemId+"&ptype=1", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	var ximalaya ximalayaData
+	if err = json.Unmarshal([]byte(jsonData), &ximalaya); err != nil {
+		return nil, err
+	}
+
+	realURL := ximalaya.Data.Src
+	urlData := make([]*types.Part, 0)
+	totalSize, err := request.Size(realURL, url)
+	if err != nil {
+		return nil, err
+	}
+	_, ext, err := utils.GetNameAndExt(realURL)
+	if err != nil {
+		return nil, err
+	}
+	urlData = append(urlData, &types.Part{
+		URL:  realURL,
+		Size: totalSize,
+		Ext:  ext,
+	})
+	streams := map[string]*types.Stream{
+		"default": {
+			Parts: urlData,
+			Size:  totalSize,
+		},
+	}
+
+	return []*types.Data{
+		{
+			Site:    "喜马拉雅 ximalaya.com",
+			Title:   title,
+			Type:    types.DataTypeAudio,
+			Streams: streams,
+			URL:     url,
+		},
+	}, nil
+}

extractors/ximalaya/ximalaya_test.go
@@ -1,0 +1,30 @@
+package ximalaya
+
+import (
+	"testing"
+
+	"github.com/iawia002/annie/extractors/types"
+	"github.com/iawia002/annie/test"
+)
+
+func TestDownload(t *testing.T) {
+	tests := []struct {
+		name string
+		args test.Args
+	}{
+		{
+			name: "normal test",
+			args: test.Args{
+				URL:   "https://www.ximalaya.com/sound/211583675",
+				Title: "狼的眼睛为什么会发光 - 十万个为什么【宝宝巴士百科故事】",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			data, err := New().Extract(tt.args.URL, types.Options{})
+			test.CheckError(t, err)
+			test.Check(t, tt.args, data[0])
+		})
+	}
+}